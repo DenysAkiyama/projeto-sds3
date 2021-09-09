ceb59e67f27026bac8ac113f24692881647396d7
DenysAkiyama <denys.akiyama@gmail.com>
2021-09-08 21:29:37 -0300

    Layers

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
@@ -29,7 +29,6 @@ public class Sale {
 	public Sale() {}
 
 	public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date, Seller seller) {
-		super();
 		this.id = id;
 		this.visited = visited;
 		this.deals = deals;
